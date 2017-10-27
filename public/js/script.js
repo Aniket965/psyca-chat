051561b0db642785cec058906f14c83c02f16483
aniket965 <aniket965.as@gmail.com>
2017-10-28 04:04:55 +0530

    Updated Data Ref

--- public/js/script.js
+++ public/js/script.js
@@ -8,15 +8,26 @@ var width = 320;
 var height = 240;
 
 var detector = new affdex.CameraDetector(divRoot, width, height, faceMode)
-
+let running = false
 detector.addEventListener("onImageResultsSuccess", function (faces, image, timestamp) {
     faces.forEach((face) => {
-       console.log(face)
-       fetch(`http://localhost:3000/log?joy=${face}`)
-        .then()
+        if (!running) {
+            running = true
+            firebase.database().ref('/codeoff').push(face.emotions);
+            console.log(face.emotions)
+            setTimeout(_ => {
+
+                running = false
+            }, 1000)
+        } else {
+
+        }
+
     })
 })
+function push(emo) {
 
+}
 detector.addEventListener("onImageResultsFailure", function (image, timestamp, err_detail) {
     console.log(image)
 })
@@ -31,3 +42,17 @@ detector.addEventListener("onWebcamConnectFailure", function () {
 detector.detectAllExpressions()
 detector.detectAllEmotions()
 detector.start()
+
+$(document).ready(() => {
+    var config = {
+        apiKey: "AIzaSyCOmubrc3gEd6LOW5UfRH5LVaL-GFgRCgk",
+        authDomain: "not-so-awesome-project-45a2e.firebaseapp.com",
+        databaseURL: "https://not-so-awesome-project-45a2e.firebaseio.com",
+        projectId: "not-so-awesome-project-45a2e",
+        storageBucket: "not-so-awesome-project-45a2e.appspot.com",
+        messagingSenderId: "481329884022"
+    };
+    firebase.initializeApp(config);
+
+
+})
